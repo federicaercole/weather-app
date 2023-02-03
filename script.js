09982e49cfae8d4820d5b0b5bb9de00f62d90e59
Federica Ercole <126322565+federicaercole@users.noreply.github.com>
2023-02-03 19:21:46 +0100

    Styles for types of weather

--- script.js
+++ script.js
@@ -1,25 +1,5 @@
-const temperature = document.querySelector(".temperature");
-const weather = document.querySelector(".weather");
-const body = document.querySelector("body");
-const errorMsg = document.querySelector(".error");
-
-const button = document.querySelector("button");
-button.addEventListener("click", getWeatherData);
-
+//App
 let temperatureUnit = "celsius";
-
-const unitSelection = [...document.querySelectorAll('input[type="radio"]')];
-
-unitSelection.map(item => item.addEventListener("click", event => {
-    resetRadioButtons();
-    event.target.checked = true;
-    temperatureUnit = event.target.value;
-    getWeatherData();
-}));
-
-function resetRadioButtons() {
-    unitSelection.forEach(item => item.checked = false);
-}
 
 async function getWeatherData() {
     try {
@@ -33,6 +13,24 @@ async function getWeatherData() {
     }
 }
 
+const unitSelection = [...document.querySelectorAll('input[type="radio"]')];
+
+unitSelection.map(item => item.addEventListener("click", event => {
+    resetRadioButtons();
+    event.target.checked = true;
+    temperatureUnit = event.target.value;
+    getWeatherData();
+}));
+
+const button = document.querySelector("button");
+button.addEventListener("click", getWeatherData);
+
+//UI
+const body = document.querySelector("body");
+const errorMsg = document.querySelector(".error");
+const temperature = document.querySelector(".temperature");
+const weather = document.querySelector(".weather");
+
 function printValues(json) {
     const unitSymbol = temperatureUnit === "celsius" ? "°C" : "°F";
     temperature.textContent = `${json.current_weather.temperature}${unitSymbol}`;
@@ -40,12 +38,6 @@ function printValues(json) {
     const time = Number(json.current_weather.time.split('T')[1].split(":")[0]);
     switchWeatherConditions(weatherCode);
     switchTimeConditions(time);
-}
-
-function switchTimeConditions(time) {
-    if (time <= 6 && time >= 18) {
-        body.setAttribute("night");
-    }
 }
 
 function switchWeatherConditions(weatherCode) {
@@ -56,14 +48,17 @@ function switchWeatherConditions(weatherCode) {
             break;
         case 1:
             weather.textContent = "Mainly clear";
+            body.setAttribute("class", "clear");
             break;
         case 2:
         case 3:
             weather.textContent = "Cloudy";
+            body.setAttribute("class", "cloudy");
             break;
         case 45:
         case 48:
             weather.textContent = "Fog";
+            body.setAttribute("class", "fog");
             break;
         case 51:
         case 53:
@@ -71,6 +66,7 @@ function switchWeatherConditions(weatherCode) {
         case 56:
         case 57:
             weather.textContent = "Drizzle";
+            body.setAttribute("class", "light-rain");
             break;
         case 61:
         case 63:
@@ -78,33 +74,54 @@ function switchWeatherConditions(weatherCode) {
         case 66:
         case 67:
             weather.textContent = "Rain";
+            body.setAttribute("class", "rain");
             break;
         case 71:
         case 73:
         case 75:
             weather.textContent = "Snow fall";
+            body.setAttribute("class", "snow");
             break;
         case 77:
             weather.textContent = "Snow grains";
+            body.setAttribute("class", "snow");
             break;
         case 80:
         case 81:
         case 82:
             weather.textContent = "Rain shower";
+            body.setAttribute("class", "rain");
             break;
         case 85:
         case 86:
             weather.textContent = "Snow shower";
+            body.setAttribute("class", "snow");
             break;
         case 95:
             weather.textContent = "Thunderstorm";
+            body.setAttribute("class", "thunderstorm");
             break;
         case 96:
         case 99:
             weather.textContent = "Thunderstorm with hail";
+            body.setAttribute("class", "thunderstorm");
             break;
     }
 }
 
+
+//Helper functions
+function resetRadioButtons() {
+    unitSelection.forEach(item => item.checked = false);
+}
+
+//funzione da rivedere
+function switchTimeConditions(time) {
+    if (time >= 18 && time <= 23 || time >= 0 && time <= 6) {
+        body.classList.add("night");
+    }
+}
+
+//Start
 resetRadioButtons();
 unitSelection[0].checked = true;
