04a8f1b5d62a145831aa4494c81edd4b3ff346e1
Federica Ercole <126322565+federicaercole@users.noreply.github.com>
2023-11-02 11:41:52 +0100

    Start refactoring View and add new file for Model

--- js/app.js
+++ js/app.js
@@ -1,91 +1,103 @@
-const app = (function () {
+import Model from "./model.js";
+import View from "./view.js"
+
+function init() {
     let temperatureUnit = "celsius";
 
-    async function getSearchResult(location) {
-        ui.loader.classList.remove("hidden");
-        try {
-            const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${location}`, { mode: 'cors' });
-            const jsonData = await response.json();
-            const latitude = jsonData.results[0].latitude;
-            const longitude = jsonData.results[0].longitude;
-            const locationName = jsonData.results[0].name;
-            const country = jsonData.results[0].country;
-            getWeatherData(latitude, longitude, locationName, country);
-        } catch {
-            ui.showErrors();
-            ui.errorMsg.textContent = "The location doesn't exist or you typed it wrong. Retry";
-        } finally {
-            ui.loader.classList.add("hidden");
-        }
-    }
+    const view = new View();
+    const model = new Model();
 
-    async function getWeatherData(latitude, longitude, locationName, country) {
-        ui.loader.classList.remove("hidden");
-        try {
-            const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&timezone=auto&temperature_unit=${temperatureUnit}`, { mode: 'cors' });
-            const jsonData = await response.json();
-            const currentTemperature = jsonData.current_weather.temperature;
-            const weatherCode = jsonData.current_weather.weathercode;
-            const time = Number(jsonData.current_weather.time.split('T')[1].split(":")[0]);
-            ui.printValues(currentTemperature, temperatureUnit, weatherCode, time, locationName, country);
-        }
-        catch {
-            ui.showErrors();
-            ui.errorMsg.textContent = "Weather is not available!"
-        } finally {
-            ui.loader.classList.add("hidden");
-        }
-    }
+}
 
-    const unitSelection = [...document.querySelectorAll('input[type="radio"]')];
+init();
 
-    unitSelection.map(item => item.addEventListener("click", event => {
-        temperatureUnit = event.target.value;
-        if (temperatureUnit === "fahrenheit") {
-            const CToFDegrees = convertToFahrenheit(Number(ui.temperature.textContent.split("°")[0]));
-            ui.printTemperature(CToFDegrees, "fahrenheit");
-        } else {
-            const FToCDegrees = convertToCelsius(Number(ui.temperature.textContent.split("°")[0]));
-            ui.printTemperature(FToCDegrees, "celsius");
-        }
-    }));
+// const app = (function () {
+//
+//     async function getSearchResult(location) {
+//         ui.loader.classList.remove("hidden");
+//         try {
+//             const response = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${location}`, { mode: 'cors' });
+//             const jsonData = await response.json();
+//             const latitude = jsonData.results[0].latitude;
+//             const longitude = jsonData.results[0].longitude;
+//             const locationName = jsonData.results[0].name;
+//             const country = jsonData.results[0].country;
+//             getWeatherData(latitude, longitude, locationName, country);
+//         } catch {
+//             ui.showErrors();
+//             ui.errorMsg.textContent = "The location doesn't exist or you typed it wrong. Retry";
+//         } finally {
+//             ui.loader.classList.add("hidden");
+//         }
+//     }
 
-    const input = document.querySelector('input[type="text"]');
+//     async function getWeatherData(latitude, longitude, locationName, country) {
+//         ui.loader.classList.remove("hidden");
+//         try {
+//             const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&timezone=auto&temperature_unit=${temperatureUnit}`, { mode: 'cors' });
+//             const jsonData = await response.json();
+//             const currentTemperature = jsonData.current_weather.temperature;
+//             const weatherCode = jsonData.current_weather.weathercode;
+//             const time = Number(jsonData.current_weather.time.split('T')[1].split(":")[0]);
+//             ui.printValues(currentTemperature, temperatureUnit, weatherCode, time, locationName, country);
+//         }
+//         catch {
+//             ui.showErrors();
+//             ui.errorMsg.textContent = "Weather is not available!"
+//         } finally {
+//             ui.loader.classList.add("hidden");
+//         }
+//     }
 
-    input.addEventListener("keydown", event => {
-        if (event.key === "Enter") {
-            search(event);
-        }
-    })
+//     const unitSelection = [...document.querySelectorAll('input[type="radio"]')];
 
-    const button = document.querySelector("button");
-    button.addEventListener("click", (event) => {
-        search(event);
-    });
+//     unitSelection.map(item => item.addEventListener("click", event => {
+//         temperatureUnit = event.target.value;
+//         if (temperatureUnit === "fahrenheit") {
+//             const CToFDegrees = convertToFahrenheit(Number(ui.temperature.textContent.split("°")[0]));
+//             ui.printTemperature(CToFDegrees, "fahrenheit");
+//         } else {
+//             const FToCDegrees = convertToCelsius(Number(ui.temperature.textContent.split("°")[0]));
+//             ui.printTemperature(FToCDegrees, "celsius");
+//         }
+//     }));
 
-    function search(event) {
-        ui.resetErrors();
-        ui.weatherData.classList.add("hidden");
-        if (input.validity.valueMissing) {
-            ui.showErrors();
-            event.preventDefault();
-        } else {
-            getSearchResult(input.value);
-            input.value = "";
-        }
-    }
+//     const input = document.querySelector('input[type="text"]');
 
-    return { unitSelection, temperatureUnit, input, getWeatherData };
-})();
+//     input.addEventListener("keydown", event => {
+//         if (event.key === "Enter") {
+//             search(event);
+//         }
+//     })
 
-const convertToCelsius = function (degree) {
-    let conversion = ((degree - 32) * (5 / 9));
-    return parseFloat(conversion.toFixed(1));
-};
+//     const button = document.querySelector("button");
+//     button.addEventListener("click", (event) => {
+//         search(event);
+//     });
 
-const convertToFahrenheit = function (degree) {
-    let conversion = (degree * (9 / 5) + 32);
-    return parseFloat(conversion.toFixed(1));
-};
+//     function search(event) {
+//         ui.resetErrors();
+//         ui.weatherData.classList.add("hidden");
+//         if (input.validity.valueMissing) {
+//             ui.showErrors();
+//             event.preventDefault();
+//         } else {
+//             getSearchResult(input.value);
+//             input.value = "";
+//         }
+//     }
 
-app.unitSelection[0].checked = true;
+//     return { unitSelection, temperatureUnit, input, getWeatherData };
+// })();
+
+// const convertToCelsius = function (degree) {
+//     let conversion = ((degree - 32) * (5 / 9));
+//     return parseFloat(conversion.toFixed(1));
+// };
+
+// const convertToFahrenheit = function (degree) {
+//     let conversion = (degree * (9 / 5) + 32);
+//     return parseFloat(conversion.toFixed(1));
+// };
+
+// app.unitSelection[0].checked = true;
--- js/view.js
+++ js/view.js
@@ -1,121 +1,120 @@
-const ui = (function () {
-    const body = document.querySelector("body");
-    const temperature = document.querySelector(".temperature");
-    const weather = document.querySelector(".weather");
-    const errorMsg = document.querySelector(".error");
-    const weatherData = document.querySelector(".weather-data")
-    const loader = document.querySelector(".loader");
 
-    function printTemperature(degree, temperatureUnit) {
-        let unitSymbol;
-        const span = document.createElement("span");
-        temperatureUnit === "celsius" ? unitSymbol = "C" : unitSymbol = "F";
-        temperature.textContent = `${degree}°`;
-        span.textContent = unitSymbol;
-        temperature.appendChild(span);
+export default class View {
+
+    nodes = {}
+
+    constructor() {
+        this.nodes.body = this.#$("body");
+        this.nodes.temperature = this.#$(".temperature");
+        this.nodes.weather = this.#$(".weather");
+        this.nodes.weatherData = this.#$(".weather-data");
+        this.nodes.errorMsg = this.#$(".error");
+        this.nodes.loader = this.#$(".loader");
     }
 
-    function printValues(temperature, temperatureUnit, weatherCode, time, locationName, country) {
-        const h2 = document.querySelector("h2");
-        const span = document.createElement("span");
-        h2.textContent = `${locationName} - `;
+    #$ = document.querySelector.bind(document);
+    #createElement = document.createElement.bind(document);
+
+    printValues(object) {
+        const { temperature, temperatureUnit, weatherCode, time, locationName, country } = object;
+        const title = this.#$("h2");
+        const span = this.#createElement("span");
+        title.textContent = `${locationName} - `;
         span.textContent = country;
         h2.appendChild(span);
-        printTemperature(temperature, temperatureUnit);
-        switchWeatherClass(weatherCode);
-        switchTimeClass(time);
-        weatherData.classList.remove("hidden");
+        // printTemperature(temperature, temperatureUnit);
+        this.#switchWeatherClass(weatherCode);
+        // switchTimeClass(time);
+        this.nodes.weatherData.classList.remove("hidden");
     }
 
-    function switchWeatherClass(weatherCode) {
+    // function printTemperature(degree, temperatureUnit) {
+    //     let unitSymbol;
+    //     const span = document.createElement("span");
+    //     temperatureUnit === "celsius" ? unitSymbol = "C" : unitSymbol = "F";
+    //     temperature.textContent = `${degree}°`;
+    //     span.textContent = unitSymbol;
+    //     temperature.appendChild(span);
+    // }
+
+    #setUI(text, ...classes) {
+        this.nodes.weather.textContent = text;
+        this.nodes.body.setAttribute("class", "");
+        classes.map(item => this.nodes.body.classList.add(item));
+    }
+
+    #switchWeatherClass(weatherCode) {
         switch (weatherCode) {
             case 0:
-                weather.textContent = "Clear sky";
-                body.setAttribute("class", "clear")
+                this.#setUI("Clear sky", "clear");
                 break;
             case 1:
-                weather.textContent = "Mainly clear";
-                body.setAttribute("class", "clear");
+                this.#setUI("Mainly clear", "clear");
                 break;
             case 2:
             case 3:
-                weather.textContent = "Cloudy";
-                body.setAttribute("class", "cloudy");
+                this.#setUI("Cloudy", "cloudy");
                 break;
             case 45:
             case 48:
-                weather.textContent = "Fog";
-                body.setAttribute("class", "fog");
-                body.classList.add("dark-text");
+                this.#setUI("Fog", "fog", "dark-text");
                 break;
             case 51:
             case 53:
             case 55:
             case 56:
             case 57:
-                weather.textContent = "Drizzle";
-                body.setAttribute("class", "light-rain");
+                this.#setUI("Drizzle", "light-rain");
                 break;
             case 61:
             case 63:
             case 65:
             case 66:
             case 67:
-                weather.textContent = "Rain";
-                body.setAttribute("class", "rain");
+                this.#setUI("Rain", "rain");
                 break;
             case 71:
             case 73:
             case 75:
-                weather.textContent = "Snow fall";
-                body.setAttribute("class", "snow");
-                body.classList.add("dark-text");
+                this.#setUI("Snow fall", "snow", "dark-text");
                 break;
             case 77:
-                weather.textContent = "Snow grains";
-                body.setAttribute("class", "snow");
-                body.classList.add("dark-text");
+                this.#setUI("Snow grains", "snow", "dark-text");
                 break;
             case 80:
             case 81:
             case 82:
-                weather.textContent = "Rain shower";
-                body.setAttribute("class", "rain");
+                this.#setUI("Rain shower", "rain");
                 break;
             case 85:
             case 86:
-                weather.textContent = "Snow shower";
-                body.setAttribute("class", "snow");
+                this.#setUI("Snow shower", "snow");
                 break;
             case 95:
-                weather.textContent = "Thunderstorm";
-                body.setAttribute("class", "thunderstorm");
+                this.#setUI("Thunderstorm", "thunderstorm");
                 break;
             case 96:
             case 99:
-                weather.textContent = "Thunderstorm with hail";
-                body.setAttribute("class", "thunderstorm");
+                this.#setUI("Thunderstorm with hail", "thunderstorm");
                 break;
         }
     }
+}
 
-    function switchTimeClass(time) {
-        if (time >= 18 && time <= 23 || time >= 0 && time <= 6) {
-            body.classList.add("night");
-        }
-    }
+// function switchTimeClass(time) {
+//     if (time >= 18 && time <= 23 || time >= 0 && time <= 6) {
+//         body.classList.add("night");
+//     }
+// }
 
-    function showErrors() {
-        errorMsg.classList.remove("hidden");
-        if (app.input.validity.valueMissing) {
-            errorMsg.textContent = "Write a location";
-        }
-    }
+// function showErrors() {
+//     errorMsg.classList.remove("hidden");
+//     if (app.input.validity.valueMissing) {
+//         errorMsg.textContent = "Write a location";
+//     }
+// }
 
-    function resetErrors() {
-        errorMsg.classList.add("hidden");
-        errorMsg.textContent = "";
-    }
-
-    return { printValues, printTemperature, showErrors, resetErrors, errorMsg, temperature, weatherData, loader }
-})();
+// function resetErrors() {
+//     errorMsg.classList.add("hidden");
+//     errorMsg.textContent = "";
+// }
